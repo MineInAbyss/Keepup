2c5531424387a0ec4411c0cc30117da76c1b9c81
0ffz <dan.voznyy@gmail.com>
2022-12-12 00:40:24 -0500

    Add option to ignore similar files, clean up and document helpers

--- src/main/kotlin/Helpers.kt
+++ src/main/kotlin/Helpers.kt
@@ -1,9 +1,22 @@
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.ConfigRenderOptions
+import commands.DownloadedItem
+import commands.Rclone
+import commands.Wget
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 import java.nio.file.Path
-import kotlin.io.path.deleteIfExists
-import kotlin.io.path.isDirectory
-import kotlin.io.path.isSymbolicLink
-import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.*
 
+/** Convers a HOCON [input] stream into JSON */
+fun renderHocon(input: InputStream): ByteArrayInputStream {
+    val config = ConfigFactory.parseReader(input.reader()).resolve()
+    // Convert config to json
+    return config.root().render(ConfigRenderOptions.defaults().setComments(false).setOriginComments(false))
+        .byteInputStream()
+}
+
+/** Removes any symlinks contained in [path] */
 fun clearSymlinks(path: Path) {
     // For each file in the directory
     path.listDirectoryEntries().forEach {
@@ -11,6 +24,19 @@ fun clearSymlinks(path: Path) {
     }
 }
 
+/**
+ * Downloads a file from a [source] definition into a [targetDir]
+ *
+ * @param source Takes form of an https url, rclone remote, or `.` to ignore
+ */
+fun download(source: String, targetDir: Path): List<DownloadedItem> = when {
+    source == "." -> emptyList()
+    source.matches("^https?://.*".toRegex()) -> listOfNotNull(Wget(source, targetDir))
+    else -> Rclone.sync(source, targetDir)
+}
+
+
+/** Fold leaf Strings of [map] into a list of Strings */
 fun getLeafStrings(map: Map<String, Any?>, acc: MutableMap<String, String> = mutableMapOf()): Map<String, String> {
     map.entries.forEach { (key, value) ->
         when (value) {
@@ -20,3 +46,16 @@ fun getLeafStrings(map: Map<String, Any?>, acc: MutableMap<String, String> = mut
     }
     return acc
 }
+
+/** Creates a symlink for a downloaded [item] to a [dest] folder */
+fun linkToDest(dest: Path, isolatedPath: Path, item: DownloadedItem) {
+    (dest / item.name).createSymbolicLinkPointingTo(
+        (isolatedPath / item.relativePath).relativeTo(dest.absolute())
+    )
+}
+
+/** Removes everything between the first digit and ext of the file */
+fun String.removeVersion() = "${takeWhile { !it.isDigit() }}.${takeLastWhile { it != '.' }}"
+
+/** Checks if two strings are similar with their versions removed */
+fun similar(a: String, b: String): Boolean = a.removeVersion() == b.removeVersion()
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,46 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.choice
 import com.github.ajalt.clikt.parameters.types.inputStream
 import com.github.ajalt.clikt.parameters.types.path
 import com.jayway.jsonpath.JsonPath
-import com.typesafe.config.ConfigFactory
-import com.typesafe.config.ConfigRenderOptions
-import commands.DownloadedItem
-import commands.Rclone
-import commands.Wget
-import java.nio.file.Path
 import kotlin.io.path.*
 
 class Keepup : CliktCommand() {
-    // make path the first argument
+    // === Arguments ===
     val input by argument(help = "Path to the file").inputStream()
-    val jsonPath by option(help = "JsonPath to the root value to keep")
-        .default("$")
-    val fileType by option().choice("json", "hocon").default("hocon")
+
     val downloadPath by argument(help = "Path to download files to")
         .path(mustExist = true, canBeFile = false)
+
     val dest by argument()
         .path(mustExist = true, canBeFile = false)
 
-    fun download(source: String, targetDir: Path): List<DownloadedItem> = when {
-        source == "." -> emptyList()
-        source.matches("^https?://.*".toRegex()) -> listOfNotNull(Wget(source, targetDir))
-        else -> Rclone.sync(source, targetDir)
-    }
+    // === Options ===
+    val jsonPath by option(help = "JsonPath to the root value to keep")
+        .default("$")
+
+    val fileType by option(help = "Type of file for the input stream")
+        .choice("json", "hocon")
+        .default("hocon")
+
+    val ignoreSimilar by option(help = "Don't create symlinks for files with matching characters before the first number")
+        .flag(default = true)
 
     override fun run() {
-        val jsonInput = if(fileType == "hocon") {
+        val jsonInput = if (fileType == "hocon") {
             println("Converting HOCON to JSON")
-            val config = ConfigFactory.parseReader(input.reader()).resolve()
-            // Convert config to json
-            config.root().render(ConfigRenderOptions.defaults().setComments(false).setOriginComments(false)).byteInputStream()
+            renderHocon(input)
         } else input
 
         println("Parsing input")
         val parsed = JsonPath.parse(jsonInput)
         val items = parsed.read<Map<String, Any?>>(jsonPath)
-        // Fold leaf strings into a list
         val strings = getLeafStrings(items)
 
         println("Clearing symlinks")
@@ -52,14 +50,17 @@ class Keepup : CliktCommand() {
         strings.forEach { (key, source) ->
             val isolatedPath = (downloadPath / key).absolute()
             isolatedPath.createDirectories()
-            download(source, isolatedPath).forEach {
-                (dest / it.name).createSymbolicLinkPointingTo((isolatedPath / it.relativePath).relativeTo(dest.absolute()))
+            val files = dest.listDirectoryEntries().filter { it.isRegularFile() }
+            download(source, isolatedPath).forEach download@{ item ->
+                if (ignoreSimilar && files.any { similar(item.name, it.name) }) {
+                    println("Skipping ${item.name} because it is similar to an existing file")
+                    return@download
+                }
+                linkToDest(dest, isolatedPath, item)
             }
         }
         println("Keepup done!")
     }
 }
 
-fun main(args: Array<String>) {
-    Keepup().main(args)
-}
+fun main(args: Array<String>) = Keepup().main(args)
