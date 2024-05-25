62d7a43919b8439a160bcb74631d73f66b22a4fb
Danielle Voznyy <dan.voznyy@gmail.com>
2024-05-25 16:22:35 -0400

    chore: Move to gradle version catalogs chore: Update deps, Kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.9.21"
-    kotlin("plugin.serialization") version "1.9.21"
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.kotlinx.serialization)
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.github.ben-manes.versions") version "0.41.0"
+    id("nl.littlerobots.version-catalog-update") version "0.8.4"
     application
 }
 
@@ -13,23 +13,24 @@ repositories {
     mavenCentral()
 }
 
-tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        freeCompilerArgs = freeCompilerArgs + "-Xcontext-receivers"
+kotlin {
+    jvmToolchain(17)
+
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xcontext-receivers")
     }
 }
 
 dependencies {
-    implementation("com.typesafe:config:1.4.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
-    implementation("com.github.ajalt.clikt:clikt:3.5.0")
-    implementation("com.lordcodes.turtle:turtle:0.8.0")
-    implementation("io.ktor:ktor-client-core:2.3.8")
-    implementation("io.ktor:ktor-client-cio:2.3.8")
-    implementation("io.ktor:ktor-client-cio-jvm:2.3.8")
-    implementation("com.github.ajalt.mordant:mordant:2.3.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC2")
-    implementation("org.slf4j:slf4j-nop:2.0.12")
+    implementation(libs.hocon)
+    implementation(libs.kotlinx.serialization)
+    implementation(libs.clikt)
+    implementation(libs.turtle)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.mordant)
+    implementation(libs.kotlinx.coroutines)
+    implementation(libs.slf4j)
     testImplementation(kotlin("test"))
 }
 
@@ -41,14 +42,29 @@ application {
     mainClass.set("MainKt")
 }
 
-kotlin {
-    jvmToolchain(17)
-}
-
 tasks {
     shadowJar {
         minimize {
             exclude { it.moduleGroup == "org.slf4j" }
         }
+    }
+}
+versionCatalogUpdate {
+    this.keep {
+        keepUnusedPlugins = true
+        keepUnusedVersions = true
+        keepUnusedLibraries = true
+    }
+}
+
+tasks.dependencyUpdates {
+    fun isNonStable(version: String): Boolean {
+        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+        val isStable = stableKeyword || regex.matches(version)
+        return isStable.not()
+    }
+    rejectVersionIf {
+        isNonStable(candidate.version)
     }
 }
