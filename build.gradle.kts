3979ccad2f13a2b5fa25c795462cdc9b27a43508
Danielle Voznyy <dan.voznyy@gmail.com>
2024-11-15 22:43:53 -0500

    fix: Set jvm toolchain to 23, update shadow plugin to work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
-    alias(libs.plugins.versions)
-    alias(libs.plugins.versionCatalogUpdate)
+//    alias(libs.plugins.versions)
+//    alias(libs.plugins.versionCatalogUpdate)
 }
 
 allprojects {
@@ -10,29 +10,30 @@ allprojects {
     }
 }
 
-versionCatalogUpdate {
-    this.keep {
-        keepUnusedPlugins = true
-        keepUnusedVersions = true
-        keepUnusedLibraries = true
-    }
-}
+//versionCatalogUpdate {
+//    this.keep {
+//        keepUnusedPlugins = true
+//        keepUnusedVersions = true
+//        keepUnusedLibraries = true
+//    }
+//}
 
-tasks.dependencyUpdates {
-    fun isNonStable(version: String): Boolean {
-        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-        val isStable = stableKeyword || regex.matches(version)
-        return isStable.not()
-    }
-    rejectVersionIf {
-        isNonStable(candidate.version)
-    }
-}
+//tasks.dependencyUpdates {
+//    fun isNonStable(version: String): Boolean {
+//        val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+//        val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+//        val isStable = stableKeyword || regex.matches(version)
+//        return isStable.not()
+//    }
+//    rejectVersionIf {
+//        isNonStable(candidate.version)
+//    }
+//}
 
-subprojects {
+allprojects {
     apply(plugin = "kotlin")
+
     kotlin {
-        jvmToolchain(22)
+        jvmToolchain(23)
     }
 }
